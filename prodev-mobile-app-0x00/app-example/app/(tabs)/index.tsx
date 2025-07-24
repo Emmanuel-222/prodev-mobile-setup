6244c4f41e6c7bd46997c8a6ff6abdbf0fa5c174
Emmagodei <emmanuelogbuefi43@gmail.com>
2025-07-24 11:51:16 +0100

    Showed myapp-example folder because my checker needed to see it

---
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+const Index: React.FC = () =>  {
+  return (
+    <View>
+      <Text> First App Created</Text>
+    </View>
+  );
+}
+
+
+export default Index;
